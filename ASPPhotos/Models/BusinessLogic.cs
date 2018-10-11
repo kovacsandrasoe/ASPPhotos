76f932dd1ed8ed716ea7c8e65ffb61036b3bcc72
kovacsandrasoe <kovacs.andras@nik.uni-obuda.hu>
2018-10-11 17:57:26 +0200

    #4: BL alap

---
+++ ASPPhotos/Models/BusinessLogic.cs
@@ -0,0 +1,33 @@
+﻿using ASPPhotos.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPPhotos.Models
+{
+    public class BusinessLogic
+    {
+        PhotoContext adatbazias;
+
+        public BusinessLogic(PhotoContext db)
+        {
+            this.adatbazias = db;
+        }
+
+        public void AddPhoto(PhotoViewModel viewmodel)
+        {
+            //vm -> m
+        }
+
+        public IEnumerable<PhotoModel> GetAll()
+        {
+
+        }
+
+        public PhotoModel GetPhotoById(int id)
+        {
+
+        }
+    }
+}
